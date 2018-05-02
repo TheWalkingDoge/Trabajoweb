50eb6ab56af9f96e846d4b0091b50f0a74a473a6
antynaty <n.vergara92@gmail.com>
2018-05-01 23:38:41 -0300

    MODIFICADO paseador.js y pequeño ARREGLO en bin/www

--- routes/paseador.js
+++ routes/paseador.js
@@ -7,7 +7,6 @@ router.post('/', async (req, res, next) => {
     const apellido = req.body['apellido'];
     const telefono = req.body['telefono'];
     const password = req.body['password'];
-    const email = req.body['email'];
     if (nombre && apellido && telefono && password) {
         models.user.create({
             nombre: nombre,
@@ -50,8 +49,7 @@ router.post('/assign', async (req, res, next) => {
     const apellido = req.body.apellido;
     const telefono = req.body.telefono;
     const password = req.body.paseador;
-    const email = req.body.email;
-    if (nombre && apellido && telefono && password && email) {
+    if (nombre && apellido && telefono && password) {
 	models.paseador.findOne({
             where: {
                 telefono: telefono
@@ -137,47 +135,6 @@ router.get('/all', async (req, res, next) => {
             description: error.toString()
         });
     });
-});
-/* GET paseador listing.
-
-    Example: /paseador/lindorfo@laleydelaselva.cl
-
- */
-router.get('/:email', async (req, res, next) => {
-    const email = req.params.email;
-    if (email) {
-        models.paseador.findOne({
-            where: {
-                email: email
-            }
-        }).then(paseador => {
-            if (paseador) {
-                res.json({
-                    status: 1,
-                    statusCode: 'paseador/found',
-                    data: user.toJSON()
-                });
-            } else {
-                res.status(400).json({
-                    status: 0,
-                    statusCode: 'paseador/not-found',
-                    description: 'El mail que ha ingresado no corresponde a ningún paseador'
-                });
-            }
-        }).catch(error => {
-            res.status(400).json({
-                status: 0,
-                statusCode: 'database/error',
-                description: error.toString()
-            });
-        });
-    } else {
-        res.status(400).json({
-            status: 0,
-            statusCode: 'paseador/wrong-email',
-            description: 'Reingrese el email!'
-        });
-    }
 });
 /* GET paseador listing.
 
