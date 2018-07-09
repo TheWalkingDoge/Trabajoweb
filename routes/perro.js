d9945db8b4ac0f74671428428e1dfab4f440da80
rudolph93 <rudolph1293@gmail.com>
2018-07-09 01:00:59 -0400

    Metodo listar perro en base a mail y pass

--- routes/perro.js
+++ routes/perro.js
@@ -220,6 +220,55 @@ router.get('/all/:id', async (req, res, next) => {
     });
 });
 
+router.get('/all/', async (req, res, next) => {
+    const email = req.headers.email;
+    const password = req.headers.password;
+    models.user.findOne({
+            // attributes: [
+            //     'id'
+            // ],
+            where: {
+                email: email,
+                password: password
+            }
+        }).then(userencontrado => {
+            if(userencontrado){
+                
+                const iddueno = userencontrado.id;
+                console.log(iddueno);
+                models.perro.findAll({
+                    where: {
+                    UserId: iddueno
+                    }
+                }).then(perroencontrado => {
+                    res.json({
+                    status: 1,
+                    statusCode: 'perros/found',
+                    data: perroencontrado
+                    });   
+            });
+
+            }
+            else {
+                res.status(400).json({
+                    status: 0,
+                    statusCode: 'paseador/not-found',
+                    description: 'No se encontro al user'
+                });
+            }
+            }).catch(error => {
+                res.status(400).json({
+                status: 0,
+                statusCode: 'database/error',
+                description: error.toString()
+        });
+    });
+});
+
+
+
+
+
 /* GET perro listing.
 
     Example: /nombre/firulais
